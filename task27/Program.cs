adc83d17f5a9b8f42efd5e8a0678df2c3b3c4feb
warfa@LAPTOP-BGKII3AT <warfacesdr@mail.ru>
2023-01-17 21:38:22 +0800

    конец

---
+++ task27/Program.cs
@@ -0,0 +1,17 @@
+﻿//Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+//452 -> 11
+//82 -> 10
+//9012 -> 12
+Console.WriteLine("Введите целое число ");
+int number = Convert.ToInt32(Console.ReadLine());
+{
+    int sum = 0;
+    int temp = 0;
+    while (number > 0)
+    {
+        temp = number % 10;
+        sum = sum + temp;
+        number = number / 10;
+    }
+    Console.WriteLine($"Сумма чисел числа равна -> {sum}");
+}
